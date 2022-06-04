01a9e3242ab55a62136173cd41d05129272a1e90
FerraroSoftware <software.ferraro@gmail.com>
2022-06-04 18:17:31 -0400

    inital setup

---
+++ contracts/Raffle.sol
@@ -0,0 +1,14 @@
+// Raffle
+// Enter lottery (pay x amount)
+// Pick random winner (verifiably random)
+// winner to be selected every x minutes -> completely automated
+// Chainlink oracle -> randomness, automated execution (chainlink keepers)
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Raffle {
+    function enterRaffle() {}
+
+    function pickRandomWinner() {}
+}
