03f8c395b6e0df46978efced8ae974c08ff156e6
FerraroSoftware <software.ferraro@gmail.com>
2022-06-04 18:51:25 -0400

    enter raffle and event/emit functionality

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -7,8 +7,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract Raffle {
-    function enterRaffle() {}
+error Raffle__NotEnoughETHEntered();
 
-    function pickRandomWinner() {}
+contract Raffle {
+    /* State variables */
+    uint256 private immutable i_entraceFee;
+    address payable[] private s_players; // someone from this list will need to be paid
+    /* Events */
+    event RaffleEnter(address indexed player);
+
+    constructor(uint256 entraceFee) {
+        i_entraceFee = entraceFee;
+    }
+
+    function enterRaffle() public payable {
+        // require (msg.value > i_entraceFee, "not enough eth") -> not as gas effective, storing string
+        if (msg.value < i_entraceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+        // Events - emit an event when we update a dynamic array or mapping
+        // Named events with the function name reversed
+        emit RaffleEnter(msg.sender);
+    }
+
+    //  function pickRandomWinner() {}
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entraceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
 }
