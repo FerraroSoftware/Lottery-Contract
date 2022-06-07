bd49537585bb7eeac4807c221103c167c6bc9a3c
FerraroSoftware <software.ferraro@gmail.com>
2022-06-07 07:52:33 -0400

    clean up

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -1,41 +1,3 @@
-// const { assert, expect } = require("chai");
-// const { network, deployments, ethers, getNamedAccounts } = require("hardhat");
-// const {
-//   developmentChains,
-//   networkConfig,
-// } = require("../../helper-hardhat-config");
-
-// // Only run on development chains
-// !developmentChains.includes(network.name)
-//   ? describe.skip
-//   : describe("Raffle", async function () {
-//       let raffle, vrfCoordinatorV2Mock;
-//       const chainId = network.config.chainId;
-
-//       beforeEach(async function () {
-//         const { deployer } = await getNamedAccounts();
-//         // all tag from deploy scripts
-//         await deployments.fixture("[all");
-//         raffle = await ethers.getContract("Raffle", deployer);
-//         vrfCoordinatorV2Mock = await ethers.getContract(
-//           "VRFCoordinatorV2Mock",
-//           deployer
-//         );
-//       });
-
-//       describe("constuctor", async function () {
-//         it("initializes the raffle correctly", async function () {
-//           // Ideally we make our tests have just 1 assert per "it"
-//           const raffleState = await raffle.getRaffleState();
-//           const interval = await raffle.getInterval();
-//           assert.equal(raffleState.toString(), "0");
-//           assert.equal(
-//             interval == networkConfig[chainId]["keepersUpdateInterval"]
-//           );
-//         });
-//       });
-//     });
-
 const { assert, expect } = require("chai");
 const { network, deployments, ethers, getNamedAccounts } = require("hardhat");
 const {
@@ -43,16 +5,15 @@ const {
   networkConfig,
 } = require("../../helper-hardhat-config");
 
+// Only run on dev chains
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe("Raffle Unit Tests", function () {
       let raffle, vrfCoordinatorV2Mock;
 
       beforeEach(async () => {
-        //   accounts = await ethers.getSigners(); // could also do with getNamedAccounts
-        //   deployer = accounts[0]
-        //   player = accounts[1];
         const { deployer } = await getNamedAccounts();
+        // from tags
         await deployments.fixture(["all"]);
 
         raffle = await ethers.getContract("Raffle", deployer);
@@ -60,7 +21,6 @@ const {
           "VRFCoordinatorV2Mock",
           deployer
         );
-        //   raffle = raffleContract.connect(player);
       });
 
       describe("constructor", function () {
