29066b0ee09ff0b10cc07acb72a187dacbad0423
FerraroSoftware <software.ferraro@gmail.com>
2022-06-08 19:24:46 -0400

    enterraffle testing block

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -9,10 +9,11 @@ const {
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe("Raffle Unit Tests", function () {
-      let raffle, vrfCoordinatorV2Mock;
+      let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer, interval;
 
       beforeEach(async () => {
-        const { deployer } = await getNamedAccounts();
+        //   const { deployer } = await getNamedAccounts();
+        deployer = (await getNamedAccounts()).deployer;
         // from tags
         await deployments.fixture(["all"]);
 
@@ -21,6 +22,8 @@ const {
           "VRFCoordinatorV2Mock",
           deployer
         );
+        raffleEntranceFee = await raffle.getEntranceFee();
+        interval = await raffle.getInterval();
       });
 
       describe("constructor", function () {
@@ -28,12 +31,46 @@ const {
           // Ideally, we'd separate these out so that only 1 assert per "it" block
           // And ideally, we'd make this check everything
           const raffleState = await raffle.getRaffleState();
-          const interval = await raffle.getInterval();
           assert.equal(raffleState.toString(), "0");
           assert.equal(
             interval.toString(),
             networkConfig[network.config.chainId]["keepersUpdateInterval"]
           );
+        });
+      });
+
+      describe("enterRaffle", async function () {
+        it("reverts when you don't pay enough", async function () {
+          await expect(raffle.enterRaffle()).to.be.revertedWith(
+            "Raffle__NotEnoughETHEntered"
+          );
+        });
+        it("records players when they enter", async function () {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          const playerFromContract = await raffle.getPlayer(0);
+
+          // deployer and first to enter should match
+
+          assert.equal(playerFromContract, deployer);
+        });
+        it("emits event on enter", async function () {
+          await expect(
+            raffle.enterRaffle({ value: raffleEntranceFee })
+          ).to.emit(raffle, "RaffleEnter");
+        });
+
+        it("doesn't allow entrance when raffle is calculating", async function () {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() + 1,
+          ]);
+          await network.provider.send("evm_mine", []);
+          // pretend to be chainlink keeper
+          await raffle.performUpkeep([]);
+
+          await expect(
+            raffle.enterRaffle({ value: raffleEntranceFee })
+          ).to.be.revertedWith("Raffle__NotOpen");
         });
       });
     });
