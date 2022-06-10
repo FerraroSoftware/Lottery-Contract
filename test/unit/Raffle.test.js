9ed38906dcd64cdbfc6748900500fe270553b025
FerraroSoftware <software.ferraro@gmail.com>
2022-06-09 21:17:18 -0400

    adding front end compatibility

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -119,4 +119,35 @@ const {
         const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x");
         assert(upkeepNeeded);
       });
+
+      describe("performUpkeep", function () {
+        it("can only run if checkupkeep is true", async () => {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() + 1,
+          ]);
+          await network.provider.request({ method: "evm_mine", params: [] });
+          const tx = await raffle.performUpkeep("0x");
+          assert(tx);
+        });
+        it("reverts if checkup is false", async () => {
+          await expect(raffle.performUpkeep("0x")).to.be.revertedWith(
+            "Raffle__UpkeepNotNeeded"
+          );
+        });
+        it("updates the raffle state and emits a requestId", async () => {
+          // Too many asserts in this test!
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() + 1,
+          ]);
+          await network.provider.request({ method: "evm_mine", params: [] });
+          const txResponse = await raffle.performUpkeep("0x");
+          const txReceipt = await txResponse.wait(1);
+          const raffleState = await raffle.getRaffleState();
+          const requestId = txReceipt.events[1].args.requestId;
+          assert(requestId.toNumber() > 0);
+          assert(raffleState == 1);
+        });
+      });
     });
